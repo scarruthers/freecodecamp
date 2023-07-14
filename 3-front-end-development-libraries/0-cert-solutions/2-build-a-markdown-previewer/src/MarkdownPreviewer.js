54430eee820c74167e68d131c3fc1c2dc00ff2f5
Sean Carruthers <1029021+scarruthers@users.noreply.github.com>
2023-07-14 13:10:24 -0500

    updated css, included the marked library for markdown rendering, added more default markdown to meet requirements

--- 3-front-end-development-libraries/0-cert-solutions/2-build-a-markdown-previewer/src/MarkdownPreviewer.js
+++ 3-front-end-development-libraries/0-cert-solutions/2-build-a-markdown-previewer/src/MarkdownPreviewer.js
@@ -1,6 +1,30 @@
 import React from 'react'
 
 import './MarkdownPreviewer.css'
+import { marked } from 'marked'
+
+const defaultEditorValue = [
+    "# This is a test heading 1",
+    "## This is a test heading 2",
+    "This includes a [link](https://www.google.com) to Google.\r\n",
+    "Here we have **bold** and *italicized* text, or ***better yet***.\r\n",
+    "This has inline code: `const TRUE = true;`.\r\n",
+    "This is a code block:\r\n",
+    "```",
+    "const TRUE = true;",
+    "const FALSE = false;",
+    "let counter = 0;",
+    "```\r\n",
+    "> And this is a blockquote.\r\n",
+    "Here's a list:",
+    "1. Item 1",
+    "2. Item 2",
+    "",
+    "And last but not least, an image:\r\n",
+    "![Picture of code](https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Programming_code.jpg/320px-Programming_code.jpg 'Picture of code')",
+    "",
+    ">Martin Vorel, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+].join("\r\n");
 
 class MarkdownPreviewer extends React.Component {
     constructor(props) {
@@ -16,60 +40,46 @@ class MarkdownPreviewer extends React.Component {
     // Automatically render our default markdown
     // TODO set the textarea value here?
     componentDidMount() {
+        const editor = document.getElementById("editor");
+        editor.value = defaultEditorValue;
         this.setState({
-            markdown: document.getElementById("editor").value
-        })
+            markdown: defaultEditorValue
+        }, this.renderMarkdown(defaultEditorValue))
     }
 
     // Update the previewer whenever the editor is updated
     markdownUpdate(event) {
+        // Update our input state and render the markdown
         this.setState({
             markdown: event.target.value
-        })
-        console.log(this.state.markdown);
+        }, this.renderMarkdown(event.target.value));
+    }
+
+    // TODO?: sanitize input
+    renderMarkdown(markdown) {
+        document.getElementById("preview").innerHTML = marked.parse(markdown);
+
+        // Update textarea height to keep both sides uniform
+        document.getElementById("editor").style.height = document.getElementById("preview").scrollHeight + "px";
     }
 
     render() {
         return (
             <div id="full-container">
-                <h1 className="text-primary text-center">MarkdownPreviewer</h1>
                 <div className="row">
-                    <div className="col-1"></div>
+                    <div className="col-2"></div>
                     <div id="markdown-container" className="col-4">
-                        
+                        <h3 className="text-center">Your Markdown:</h3>
                         <div className="form-floating">
-                            {
-                                // TODO: figure out how to preserve the line breaks in placeholder
-                                // separate into a different react class?
-                                // incorporate \r\n somehow?
-                            }
-                            <textarea id="editor" className="form-control" onChange={this.markdownUpdate}>
-                            # This is a test heading 1
-                            ## This is a test heading 2
-                            
-                            This includes a (link)[https://www.google.com] to Google.
-
-                            This has inline code: `const TRUE = true;`.
-
-                            This is a code block:
-
-                            ```
-                            const TRUE = true;
-                            const FALSE = false;
-                            let counter = 0;
-                            ```
-                            </textarea>
-                            <label id="editor-label" for="editor">Enter your markdown here:</label>
+                            <textarea id="editor" className="form-control" onChange={this.markdownUpdate} autoCorrect="off"></textarea>
                         </div>
                     </div>
-                    <div className="col-2"></div>
+                    
                     <div id="preview-container" className="col-4">
                         <h3 className="text-center">Markdown Preview:</h3>
-                        <div id="preview">
-                            {this.state.markdown}
-                        </div>
+                        <div id="preview" className="border border-1 rounded"></div>
                     </div>
-                    <div className="col-1"></div>
+                    <div className="col-2"></div>
                 </div>
             </div>
         );
