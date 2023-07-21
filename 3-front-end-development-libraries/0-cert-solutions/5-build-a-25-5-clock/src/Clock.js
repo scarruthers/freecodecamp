6976ce019daf2d2ab8ee8de4d07cfae9e2dcf288
Sean Carruthers <1029021+scarruthers@users.noreply.github.com>
2023-07-21 15:05:30 -0500

    initial push for project 5 certification

---
+++ 3-front-end-development-libraries/0-cert-solutions/5-build-a-25-5-clock/src/Clock.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Clock.css'
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div id="clock">
+                <h1>Clock</h1>
+            </div>
+        )
+    }
+}
+
+export default Clock;
